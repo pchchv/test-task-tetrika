df4573a0481d606c303e8d9d8efd740599089ea7
pchchv <ipchchv@gmail.com>
2022-07-04 22:03:09 +0300

    Решение первой задачи(входные данные — массив)

--- task1.py
+++ task1.py
@@ -10,4 +10,28 @@ def task_string(string):
     return index + 1
 
 
+def task_array(array):
+    i = 0
+    j = len(array) - 1
+    k = (j - i) // 2  # средний элемент массива
+    while i <= j:
+        """Итеративный бинарный поиск.
+        Каждую итерацию цикла "отбрасываем" половину массива
+        """
+        if array[k] == 1:
+            i += k
+            if array[k] == 0:
+                return k
+            if array[k + 1] == 0:
+                return k + 1
+        else:
+            j -= k
+            if array[k] == 1:
+                return k
+            if array[k - 1] == 1:
+                return k - 1
+        k = i + (j - i) // 2
+
+
 print(task_string('111111111111111111111111100000000'))
+print(task_array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]))
